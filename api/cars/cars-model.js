5332763de82e5a8046fa4ef70e04da6bb95940ca
unknown <Keano@Firelink.Firelink>
2021-07-13 16:43:44 -0700

    mvp

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,26 @@
-const getAll = () => {
-  // DO YOUR MAGIC
+const db = require('../../data/db-config')
+
+module.exports = {
+  getAll,
+  getById,
+  create
 }
 
-const getById = () => {
+async const getAll = () => {
   // DO YOUR MAGIC
+  const records = await db('cars')
+  return records
 }
 
-const create = () => {
+async const getById = (id) => {
   // DO YOUR MAGIC
+  const record = await db('cars').where('id', id).first()
+  return record
+}
+
+async const create = (car) => {
+  // DO YOUR MAGIC
+  const [id] = await db('cars').insert(car)
+  const newCar = await getById(id)
+  return newCar
 }
